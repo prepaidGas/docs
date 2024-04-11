9670eb915d0abe3d7381e77a6c2fd093accaec04
Nikita Romashchenko <nikita.romashchenko@gmail.com>
2024-04-11 20:21:26 +0300

    out folder fix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
+// next.config.js
+
 const withNextra = require("nextra")({
-  //Nextra
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  unstable_staticImage: false,
 });
 
-const path = require("path");
-
-module.exports = withNextra({
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+module.exports = {
+  ...withNextra(),
+  images: {
+    unoptimized: true,
   },
-  trailingSlash: true,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  output: "export",
-});
+};
